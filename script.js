f51357c491613f5b9a70e3713b8c0d002e75b359
hudra <logdinghudra@gmail.ru>
2026-05-24 18:09:21 +0600

    задания 6

--- script.js
+++ script.js
@@ -95,3 +95,12 @@ function calc(a,b) {
 }
 console.log(calc(5,7))
 
+function getNumber() {
+    let number = prompt('Введите число');
+    if (isNaN(number)) {
+        return 'Переданный параметр не является числом'
+    } else {
+        return `${number} в кубе равняется ${number ** 3}`;
+    }
+}
+console.log(getNumber());
