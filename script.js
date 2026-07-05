430d5bf0851624a599e76d950bcc85fab5d867d3
hudra <logdinghudra@gmail.ru>
2026-07-05 21:40:26 +0600

    задание 1,2

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const numbs = [1, 5, 4, 10, 0, 3];
+for (let i=0; i<numbs.length; i++){
+    console.log (numbs[i]);
+    if(numbs [i]==10)break;
+}
+
+
+const numbs2 = [1, 5, 4, 10, 0, 3];
+let position = numbs2.indexOf(4);
+console.log (position);
