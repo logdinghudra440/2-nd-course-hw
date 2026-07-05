b6ad3a5b68ea8f1048febd7112fefbb2bf9738c9
hudra <logdinghudra@gmail.ru>
2026-07-05 22:26:50 +0600

    задание 3,4

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ for (let i=0; i<numbs.length; i++){
 const numbs2 = [1, 5, 4, 10, 0, 3];
 let position = numbs2.indexOf(4);
 console.log (position);
+
+
+const numbs3 = [1, 3, 5, 10, 20].join(' ');
+console.log (numbs3);
+
+
+const array = [];
+for (let i = 0; i < 3; i++) {        
+    const row = [];
+    for (let j = 0; j < 3; j++) {    
+        row.push(1);                
+    }
+    array.push(row);               
+}
+console.log(array);
+
+
+
