4d25d0af66a25b55f83191a7cfa05fe5c5c5862e
hudra <logdinghudra@gmail.ru>
2026-07-14 17:13:00 +0600

    Задания на игры

--- game1.js
+++ game1.js
@@ -1,34 +1,34 @@
 const button = document.querySelector('.game__content-buttom');
-button.addEventListener('click', function() {
-  const randomNumber = Math.floor(Math.random() * 100) + 1;
-  console.log('Случайное число: ' + randomNumber);
-  console.log('Кнопка нажата!');
+button.addEventListener('click', function () {
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    console.log('Случайное число: ' + randomNumber);
+    console.log('Кнопка нажата!');
 });
 
 const quiz = [
-     {
-       question: "Какой цвет небо?",
-       options: ["1. Красный", "2. Синий", "3. Зеленый"],
-       correctAnswer: 2 
-     },
-     {
-       question: "Сколько дней в неделе?",
-       options: ["1. Шесть", "2. Семь", "3. Восемь"],
-      correctAnswer: 2
-     },
-     {
-      question: "Сколько у человека пальцев на одной руке?",
-      options: ["1. Четыре", "2. Пять", "3. Шесть"],
-      correctAnswer: 2
-     }
- ];
+    {
+        question: "Какой цвет небо?",
+        options: ["1. Красный", "2. Синий", "3. Зеленый"],
+        correctAnswer: 2
+    },
+    {
+        question: "Сколько дней в неделе?",
+        options: ["1. Шесть", "2. Семь", "3. Восемь"],
+        correctAnswer: 2
+    },
+    {
+        question: "Сколько у человека пальцев на одной руке?",
+        options: ["1. Четыре", "2. Пять", "3. Шесть"],
+        correctAnswer: 2
+    }
+];
 
- let correctCounter = 0;
- for (let i = 0; i < quiz.length; i++) {
-  const questionText = quiz[i].question + "\n" + quiz[i].options.join("\n");
-  const userAnswer = prompt(questionText);
+let correctCounter = 0;
+for (let i = 0; i < quiz.length; i++) {
+    const questionText = quiz[i].question + "\n" + quiz[i].options.join("\n");
+    const userAnswer = prompt(questionText);
 
- if (userAnswer === null) {
+    if (userAnswer === null) {
         alert("Игра прервана пользователем.");
         break;
     }
@@ -38,7 +38,7 @@ const quiz = [
     }
     if (isNaN(userAnswer)) {
         alert("Некорректный ввод. Пожалуйста, введите номер ответа.");
-        i--; 
+        i--;
         continue;
     }
     const answerNumber = parseInt(userAnswer);
@@ -47,7 +47,7 @@ const quiz = [
         i--;
         continue;
     }
-  if (userAnswer !== null && !isNaN(userAnswer)) {
+    if (userAnswer !== null && !isNaN(userAnswer)) {
         const answerNumber = parseInt(userAnswer);
         if (answerNumber === quiz[i].correctAnswer) {
             correctCounter++;
@@ -62,4 +62,3 @@ const quiz = [
 
 alert(`Количество правильных ответов: ${correctCounter}`);
 
- 
